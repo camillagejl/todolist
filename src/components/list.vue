03cb460c467c408964c405742380b134d49ff8b1
Camilla Gejl Dilling-Hansen <camilla@dilling.co>
2023-09-08 14:50:59 +0200

    Beginning of simple to do list

---
+++ src/components/list.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="new_task">
+    <input type="text"
+           v-on:keyup.enter="addTask"
+           v-model="newTask"
+    >
+  </div>
+
+  <div class="tasks">
+  <task
+      v-for="(task, index) in tasks"
+      :task="task"
+      :index="index"
+      @event="deleteTask"
+  ></task>
+  </div>
+</template>
+
+<script>
+import Task from "@/components/task.vue";
+
+export default {
+  name: "list",
+  components: {Task},
+  data() {
+    return {
+      tasks: [
+        'Laundry',
+        'Coding',
+        'Fold laundry'
+      ],
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/task.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="task">
+    <button class="delete"
+    @click="$emit('event', index)"
+    >
+      x
+  </button>
+    {{ task }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "task",
+  props: {
+    task: String,
+    index: Number
+  }
+}
+</script>
+
+<style scoped>
+.task {
+  display: block;
+}
+</style>
